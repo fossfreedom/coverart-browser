089984e37ae115f194c6e11dcf393879e58a6cb6
fossfreedom <foss.freedom@gmail.com>
2012-09-22 12:42:32 +0100

    corrected xml plugin path and installation

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 SCRIPT_NAME=`basename "$0"`
 SCRIPT_PATH=${0%`basename "$0"`}
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/coverart_browser/"  
+PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/coverart_browser/"
+GLIB_SCHEME="org.gnome.rhythmbox.plugins.coverart_browser.gschema.xml"
+GLIB_DIR="/usr/share/glib-2.0/schemas/"
 
 #build the dirs
 mkdir -p $PLUGIN_PATH
@@ -15,3 +17,7 @@ rm "${PLUGIN_PATH}${SCRIPT_NAME}"
 #install translations
 cd po; sudo ./lang.sh /usr/share/locale/
 
+#install the glib schema
+echo "Installing the glib schema (password needed)"
+sudo cp "${PLUGIN_PATH}${GLIB_SCHEME}" "$GLIB_DIR"
+sudo glib-compile-schemas "$GLIB_DIR"
