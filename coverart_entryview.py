0b1236b63685e6b72a56660a2f854d2c97261293
Agustin Carrasco <asermax@gmail.com>
2012-09-26 13:21:23 -0300

    Added a clear method and trimed blank spaces (lol, my python ide does that automatically)

--- coverart_entryview.py
+++ coverart_entryview.py
@@ -39,9 +39,9 @@ class CoverArtEntryView(RB.EntryView):
         Initializes the source.
         '''
         self.shell = shell
-    
+
         super(RB.EntryView, self).__init__(db=shell.props.db, shell_player=shell.props.shell_player, is_drag_source=True)
-            
+
         self.append_column(RB.EntryViewColumn.TRACK_NUMBER, True)
         self.append_column(RB.EntryViewColumn.GENRE, True)
         self.append_column(RB.EntryViewColumn.TITLE, True)
@@ -49,31 +49,31 @@ class CoverArtEntryView(RB.EntryView):
         self.append_column(RB.EntryViewColumn.ALBUM, True)
         self.append_column(RB.EntryViewColumn.DURATION, True)
         self.set_columns_clickable(False)
-        
+
         uim = self.shell.props.ui_manager	
         self.play_action = Gtk.Action( 'EntryViewPlay',
                                        _('Play'),
                                        _('Add selected tracks to play queue and play'),
                                        _ )
-                                   
+
         self.play_action.connect ( 'activate', self.play_tracks )
-        
+
         self.queue_action = Gtk.Action( 'EntryViewQueue',
                                        _('Queue'),
                                        _('Queue selected tracks'),
                                        _ )
-                                   
+
         self.queue_action.connect ( 'activate', self.queue_tracks )
-        
+
         self.action_group = Gtk.ActionGroup( 'CoverArtEntryViewActionGroup' )
         self.action_group.add_action( self.play_action )
         self.action_group.add_action( self.queue_action )
         uim.insert_action_group( self.action_group, -1 )
-        
+
         self.ui_id = uim.add_ui_from_string( ui_context_menu )
         self.popup_menu = uim.get_widget( "/EntryViewPopup" )
-        uim.ensure_update()  
-        
+        uim.ensure_update()
+
     def __del__(self):
         uim = self.shell.props.ui_manager
 
@@ -84,18 +84,21 @@ class CoverArtEntryView(RB.EntryView):
         del self.action_group
         del self.play_action
         del self.queue_action
-        
+
     def add_album(self, album):
         qm = RB.RhythmDBQueryModel.new_empty(self.shell.props.db)
         album.get_entries(qm)
         self.set_model(qm)
-        
+
+    def clear(self):
+        self.set_model(RB.RhythmDBQueryModel.new_empty(self.shell.props.db))
+
     def do_show_popup(self, over_entry):
         self.popup_menu.popup(None, None, None, None, 0,
             Gtk.get_current_event_time())
 
         return True
-        
+
     def play_tracks(self, entry):
         self.queue_tracks( entry )
         # Start the music
@@ -103,12 +106,12 @@ class CoverArtEntryView(RB.EntryView):
         player.stop()
         player.set_playing_source(self.shell.props.queue_source)
         player.playpause(True)
-    
+
     def queue_tracks(self, entry):
         selected = self.get_selected_entries()
         selected.reverse()
-        
+
         for entry in selected:
-            self.shell.props.queue_source.add_entry( entry, 0 ) 
+            self.shell.props.queue_source.add_entry( entry, 0 )
 
 GObject.type_register(CoverArtEntryView)
