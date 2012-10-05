096c52c3396b968131d0272df7ae2b44d6c7af10
Agustin Carrasco <asermax@gmail.com>
2012-10-05 17:40:42 -0300

    Cleaned up the prefs module

--- coverart_browser_prefs.py
+++ coverart_browser_prefs.py
@@ -23,9 +23,12 @@ from gi.repository import PeasGtk
 
 import rb
 
-class GSetting:
-    """ A python singleton """
 
+class GSetting:
+    '''
+    This class manages the differentes settings that the plugins haves to
+    access to read or write.
+    '''
     # storage for the instance reference
     __instance = None
 
@@ -33,44 +36,60 @@ class GSetting:
         """ Implementation of the singleton interface """
         # below public variables and methods that can be called for GSetting
         def __init__(self):
-            ##
-            self.Path=self._enum(   PLUGIN='org.gnome.rhythmbox.plugins.coverart_browser',
-                                    RBSOURCE='org.gnome.rhythmbox.sources')
+            '''
+            Initializes the singleton interface, asigning all the constants
+            used to access the plugin's settings.
+            '''
+            self.Path = self._enum(
+                PLUGIN='org.gnome.rhythmbox.plugins.coverart_browser',
+                RBSOURCE='org.gnome.rhythmbox.sources')
 
-            self.RBSourceKey=self._enum(VISIBLE_COLS='visible-columns')
+            self.RBSourceKey = self._enum(VISIBLE_COLS='visible-columns')
 
-            self.PluginKey=self._enum(  CUSTOM_STATUSBAR = 'custom-statusbar',
-                                        DISPLAY_TRACKS = 'display-tracks',
-                                        DISPLAY_TEXT = 'display-text',
-                                        DISPLAY_TEXT_LOADING = 'display-text-loading',
-                                        DISPLAY_TEXT_ELLIPSIZE = 'display-text-ellipsize',
-                                        DISPLAY_TEXT_ELLIPSIZE_LENGTH = 'display-text-ellipsize-length',
-                                        COVER_SIZE = 'cover-size',
-                                        PANED_POSITION = 'paned-position',
-                                        SORT_BY_ALBUM = 'sort-by-album',
-                                        SORT_BY_ARTIST = 'sort-by-artist',
-                                        DESC_SORT = 'desc-sort',
-                                        ASC_SORT = 'asc-sort'
-                                     )
+            self.PluginKey = self._enum(
+                CUSTOM_STATUSBAR='custom-statusbar',
+                DISPLAY_TRACKS='display-tracks',
+                DISPLAY_TEXT='display-text',
+                DISPLAY_TEXT_LOADING='display-text-loading',
+                DISPLAY_TEXT_ELLIPSIZE='display-text-ellipsize',
+                DISPLAY_TEXT_ELLIPSIZE_LENGTH='display-text-ellipsize-length',
+                COVER_SIZE='cover-size',
+                PANED_POSITION='paned-position',
+                SORT_BY_ALBUM='sort-by-album',
+                SORT_BY_ARTIST='sort-by-artist',
+                DESC_SORT='desc-sort',
+                ASC_SORT='asc-sort')
 
-            self.setting={}
+            self.setting = {}
 
         def get_setting(self, path):
+            '''
+            Return an instance of Gio.Settings pointing at the selected path.
+            '''
             try:
                 setting = self.setting[path]
             except:
-                self.setting[path]=Gio.Settings(path)
+                self.setting[path] = Gio.Settings(path)
                 setting = self.setting[path]
 
             return setting
 
         def get_value(self, path, key):
+            '''
+            Return the value saved on key from the settings path.
+            '''
             return self.get_setting(path)[key]
 
         def set_value(self, path, key, value):
-            self.get_setting(path)[key]=value
+            '''
+            Set the passed value to key in the settings path.
+            '''
+            self.get_setting(path)[key] = value
 
         def _enum(self, **enums):
+            '''
+            Create an enumn.
+            '''
             return type('Enum', (), enums)
 
     def __init__(self):
@@ -90,7 +109,8 @@ class GSetting:
     def __setattr__(self, attr, value):
         """ Delegate access to implementation """
         return setattr(self.__instance, attr, value)
-        
+
+
 class Preferences(GObject.Object, PeasGtk.Configurable):
     '''
     Preferences for the CoverArt Browser Plugins. It holds the settings for
@@ -114,58 +134,51 @@ class Preferences(GObject.Object, PeasGtk.Configurable):
         '''
         # create the ui
         builder = Gtk.Builder()
-        builder.add_from_file(rb.find_plugin_file(self, 'ui/coverart_browser_prefs.ui'))
+        builder.add_from_file(rb.find_plugin_file(self,
+            'ui/coverart_browser_prefs.ui'))
 
         gs = GSetting()
         # bind the toggles to the settings
         toggle_statusbar = builder.get_object('custom_statusbar_checkbox')
-        self.settings.bind( gs.PluginKey.CUSTOM_STATUSBAR,
-                            toggle_statusbar, 'active',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.CUSTOM_STATUSBAR,
+            toggle_statusbar, 'active', Gio.SettingsBindFlags.DEFAULT)
 
         toggle_tracks = builder.get_object('display_tracks_checkbox')
-        self.settings.bind( gs.PluginKey.DISPLAY_TRACKS,
-                            toggle_tracks, 'active',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.DISPLAY_TRACKS, toggle_tracks,
+        'active', Gio.SettingsBindFlags.DEFAULT)
 
         toggle_text = builder.get_object('display_text_checkbox')
-        self.settings.bind( gs.PluginKey.DISPLAY_TEXT,
-                            toggle_text, 'active',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.DISPLAY_TEXT, toggle_text, 'active',
+            Gio.SettingsBindFlags.DEFAULT)
 
         box_text = builder.get_object('display_text_box')
-        self.settings.bind( gs.PluginKey.DISPLAY_TEXT,
-                            box_text, 'sensitive',
-                            Gio.SettingsBindFlags.GET)
+        self.settings.bind(gs.PluginKey.DISPLAY_TEXT, box_text, 'sensitive',
+            Gio.SettingsBindFlags.GET)
 
         toggle_text_loading = builder.get_object(
             'display_text_loading_checkbox')
-        self.settings.bind( gs.PluginKey.DISPLAY_TEXT_LOADING,
-                            toggle_text_loading, 'active',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.DISPLAY_TEXT_LOADING,
+        toggle_text_loading, 'active', Gio.SettingsBindFlags.DEFAULT)
 
         toggle_text_ellipsize = builder.get_object(
             'display_text_ellipsize_checkbox')
-        self.settings.bind( gs.PluginKey.DISPLAY_TEXT_ELLIPSIZE,
-                            toggle_text_ellipsize, 'active',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.DISPLAY_TEXT_ELLIPSIZE,
+            toggle_text_ellipsize, 'active', Gio.SettingsBindFlags.DEFAULT)
 
         box_text_ellipsize_length = builder.get_object(
             'display_text_ellipsize_length_box')
-        self.settings.bind( gs.PluginKey.DISPLAY_TEXT_ELLIPSIZE,
-                            box_text_ellipsize_length, 'sensitive',
-                            Gio.SettingsBindFlags.GET)
+        self.settings.bind(gs.PluginKey.DISPLAY_TEXT_ELLIPSIZE,
+            box_text_ellipsize_length, 'sensitive', Gio.SettingsBindFlags.GET)
 
         spinner_text_ellipsize_length = builder.get_object(
             'display_text_ellipsize_length_spin')
-        self.settings.bind( gs.PluginKey.DISPLAY_TEXT_ELLIPSIZE_LENGTH,
-                            spinner_text_ellipsize_length, 'value',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.DISPLAY_TEXT_ELLIPSIZE_LENGTH,
+            spinner_text_ellipsize_length, 'value',
+            Gio.SettingsBindFlags.DEFAULT)
 
         cover_size_scale = builder.get_object('cover_size_adjustment')
-        self.settings.bind( gs.PluginKey.COVER_SIZE,
-                            cover_size_scale, 'value',
-                            Gio.SettingsBindFlags.DEFAULT)
+        self.settings.bind(gs.PluginKey.COVER_SIZE, cover_size_scale, 'value',
+            Gio.SettingsBindFlags.DEFAULT)
 
         # return the dialog
         return builder.get_object('main_box')
