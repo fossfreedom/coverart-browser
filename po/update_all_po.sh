a7f761f822db8913c592b2db9e63cb827885f658
fossfreedom <foss.freedom@gmail.com>
2013-01-12 18:03:46 +0000

    latest translations and merge from master

--- po/update_all_po.sh
+++ po/update_all_po.sh
@@ -3,9 +3,10 @@ echo "updating po files"
 for i in *.po; do
 	lang=`basename $i .po`
 	echo "updating $lang"
-    intltool-update --dist $lang -g package
+    intltool-update --dist $lang -g coverartbrowser
 done
 
 echo "update plugin file"
 
 intltool-merge -d . ../coverart_browser.plugin.in ../coverart_browser.plugin
+#intltool-merge -x -d . ../img/popups.xml.in ../img/popups.xml
