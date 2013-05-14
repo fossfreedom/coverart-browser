c098d0a5acee04f9f55c9764568c79871d5dea49
fossfreedom <foss.freedom@gmail.com>
2013-05-14 20:07:28 +0100

    latest changes made by coverart-search-providers

--- rb3compat.py
+++ rb3compat.py
@@ -27,13 +27,29 @@ else:
     import urllib
     from urlparse import urlparse as rb2urlparse
 
+if PYVER >= 3:
+    import http.client
+else:
+    import httplib
+    
+def responses():
+    if PYVER >=3:
+        return http.client.responses
+    else:
+        return httplib.responses
 
 def unicodestr(param, charset):
     if PYVER >=3:
         return str(param, charset)
     else:
         return unicode(param, charset)
-    
+        
+def unicodeencode(param, charset):
+    if PYVER >=3:
+        return str(param).encode(charset)
+    else:
+        return unicode(param).encode(charset)
+
 def urlparse(uri):
     if PYVER >=3:
         return urllib.parse.urlparse(uri)
@@ -46,6 +62,12 @@ def url2pathname(url):
     else:
         return urllib.url2pathname(url)
 
+def urlopen(filename):
+    if PYVER >=3:
+        return urllib.request.urlopen(filename)
+    else:
+        return urllib.urlopen(filename)
+        
 def pathname2url(filename):
     if PYVER >=3:
         return urllib.request.pathname2url(filename)
@@ -57,6 +79,25 @@ def unquote(uri):
         return urllib.parse.unquote(uri)
     else:
         return urllib.unquote(uri)
+                
+def quote(uri, safe=None):
+    if PYVER >=3:
+        if safe:
+            return urllib.parse.quote(uri,safe=safe)
+        else:
+            return urllib.parse.quote(uri)
+    else:
+        if safe:
+            return urllib.quote(uri, safe=safe)
+        else:
+            return urllib.quote(uri)
+        
+def quote_plus(uri):
+    if PYVER >=3:
+        return urllib.parse.quote_plus(uri)
+    else:
+        return urllib.quote_plus(uri)
+
         
 def is_rb3(shell):
 	if hasattr( shell.props.window, 'add_action' ):
