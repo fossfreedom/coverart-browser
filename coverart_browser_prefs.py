f9ab0104cd465a6063c2715e8385f60ac6e54064
fossfreedom <foss.freedom@gmail.com>
2014-01-15 20:46:44 +0000

    remove depreciation of dialog creation

--- coverart_browser_prefs.py
+++ coverart_browser_prefs.py
@@ -247,9 +247,12 @@ class Preferences(GObject.Object, PeasGtk.Configurable):
     def display_preferences_dialog(self, plugin):
         if self._first_run:
             self._first_run = False
-            self._dialog = Gtk.Dialog(_('Browser Preferences'), None,
-                Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT, (Gtk.STOCK_OK, Gtk.ResponseType.OK))
-                
+            #self._dialog = Gtk.Dialog(_('Browser Preferences'), None, 
+            #    Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT, (Gtk.STOCK_OK, Gtk.ResponseType.OK))
+             
+            self._dialog = Gtk.Dialog(modal=True, destroy_with_parent=True)
+            self._dialog.add_button(Gtk.STOCK_OK, Gtk.ResponseType.OK)
+            self._dialog.set_title(_('Browser Preferences')) 
             content_area = self._dialog.get_content_area()
             content_area.pack_start(self._create_display_contents(plugin), True, True, 0)
             
